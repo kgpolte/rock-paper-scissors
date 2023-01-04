8137dec144dd7ffeb76d89e1440c2d4e6029b96c
Kenny Polte <120602839+kgpolte@users.noreply.github.com>
2023-01-04 13:01:48 -0800

    Create first working version of game script

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,8 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
+
     const randomNum = Math.floor(Math.random() * 3);
     if (randomNum === 0) {
         return 'Rock';
@@ -8,3 +12,61 @@ function getComputerChoice() {
         return 'Scissors';
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    
+    const playerChoice = playerSelection.toLowerCase();
+    const computerChoice = computerSelection.toLowerCase();
+
+    if (playerChoice == 'rock') {
+        if (computerChoice == 'rock') {
+            return 'Tie!';
+        } else if (computerChoice == 'paper') {
+            computerScore = computerScore + 1;
+            return 'You lose! Paper beats rock.';
+        } else if (computerChoice == 'scissors') {
+            playerScore = playerScore + 1;
+            return 'You win! Rock beats Scissors.';
+        }
+
+    } else if (playerChoice == 'paper') {
+        if (computerChoice == 'rock') {
+            playerScore = playerScore + 1;
+            return 'You win! Paper beats rock.';
+        } else if (computerChoice == 'paper') {
+            return 'Tie!';
+        } else if (computerChoice == 'scissors') {
+            computerScore = computerScore + 1;
+            return 'You lose! Scissors beat Paper.';
+        }
+
+    } else if (playerChoice == 'scissors') {
+        if (computerChoice == 'rock') {
+            computerScore = computerScore + 1;
+            return 'You lose! Rock beats scissors.';
+        } else if (computerChoice == 'paper') {
+            playerScore = playerScore + 1;
+            return 'You win! Scissors beat Paper.';
+        } else if (computerChoice == 'scissors') {
+            return 'Tie!';
+        }
+    }
+}
+
+function game(rounds) {
+    
+    for (let i = 0; i < rounds; i++) {
+        playerSelection = prompt('Enter your choice.');
+        computerSelection = getComputerChoice();
+        console.log(`Round ${i + 1} of ${rounds}`);
+        console.log(playRound(playerSelection, computerSelection));
+        if (i < rounds - 1) {
+            console.log(`Player: ${playerScore}, Computer: ${computerScore}`);
+        }
+    }
+    
+    console.log('Final Score:');
+    console.log(`Player: ${playerScore}, Computer: ${computerScore}`);
+}
+
+game(5);
